810f1804662763f811c147499333bbade7436b31
Jerom <jeromjomanthara@gmail.com>
2024-03-18 15:37:03 +0530

    feat(templates): use django template engine to show blog names

--- base/views.py
+++ base/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+blogs = [
+    {'id':1, 'name': 'introduction to python'},
+    {'id':2, 'name': 'introduction to django'},
+    {'id':3, 'name': 'introduction to flask'},
+]
+
+
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'blogs': blogs}
+    return render(request, 'home.html', context)
 
 def blog(request):
     return render(request, 'blog.html')
