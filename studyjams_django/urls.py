2f007c78d8c9ca93332a6c41d050b9592874156e
Jerom <jeromjomanthara@gmail.com>
2024-03-18 13:49:29 +0530

    refactor(base): relocate view fucntions to base.views file

--- studyjams_django/urls.py
+++ studyjams_django/urls.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse('Hello, Django!')
-
-def blog(request):
-    return HttpResponse('This is the blog.')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('blog/', blog),
 ]
