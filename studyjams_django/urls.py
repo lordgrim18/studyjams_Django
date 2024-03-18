677fee7e42a0434bde2ddf2df62b6981f2d0144a
Jerom <jeromjomanthara@gmail.com>
2024-03-18 13:46:44 +0530

    feat(urls): add blog url

--- studyjams_django/urls.py
+++ studyjams_django/urls.py
@@ -5,7 +5,11 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('Hello, Django!')
 
+def blog(request):
+    return HttpResponse('This is the blog.')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('blog/', blog),
 ]
