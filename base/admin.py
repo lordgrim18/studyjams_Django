c395bb59d95f701fcf0367b95c74583e32de53d7
Jerom <jeromjomanthara@gmail.com>
2024-03-19 18:46:21 +0530

    feat(base.models): add topic and comment and update blog

--- base/admin.py
+++ base/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Blog
+from .models import Blog, Topic, Comment
 
 admin.site.register(Blog)
+admin.site.register(Topic)
+admin.site.register(Comment)
--- base/models.py
+++ base/models.py
@@ -1,14 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
-    # author
-    # topic
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[:50]
