2369f71da8de584e83cdb87fbc5949306c172f8a
Jerom <jeromjomanthara@gmail.com>
2024-03-19 19:00:52 +0530

    feat(base.templates): display comments at respective blogs

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Blog
+from .models import Blog, Comment
 
 
 def home(request):
@@ -12,7 +12,9 @@ def home(request):
 
 def blog(request, pk):
     blog = Blog.objects.get(id=pk)
+    comments = Comment.objects.filter(blog=blog)  # Filter comments for this blog
     context = {
-        'blog': blog
+        'blog': blog,
+        'comments': comments,  # Add comments to the context
     }
     return render(request, 'base/blog.html', context)
