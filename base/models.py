0a725002fd6a5c377e1d0d0f4cb5c9ba7dc3471c
Jerom <jeromjomanthara@gmail.com>
2024-03-19 18:02:41 +0530

    feat(base.models): add blog model and make migrations and migrate

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    # author
+    # topic
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
