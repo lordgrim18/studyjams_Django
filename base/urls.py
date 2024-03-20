ed1978527dc5949abf8d048f8ab816d79e825aa4
Jerom <jeromjomanthara@gmail.com>
2024-03-20 11:16:46 +0530

    feat(base.urls): add create blog url

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('blog/<str:pk>/', views.blog, name='blog'),
+
+    path('create-blog/', views.createBlog, name='create-blog'),
 ]
