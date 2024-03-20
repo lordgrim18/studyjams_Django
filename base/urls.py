37531207bd7ccdecc09413900767bde7c5c8d60d
Jerom <jeromjomanthara@gmail.com>
2024-03-20 18:36:40 +0530

    perf(base): replace function based views with class based views

--- base/urls.py
+++ base/urls.py
@@ -3,11 +3,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('blog/<str:pk>/', views.blog, name='blog'),
+    # path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
+    # path('blog/<str:pk>/', views.blog, name='blog'),
+    path('blog/<str:pk>/', views.BlogView.as_view(), name='blog'),
 
-    path('create-blog/', views.createBlog, name='create-blog'),
-    path('update-blog/<str:pk>/', views.updateBlog, name='update-blog'),
-    path('delete-blog/<str:pk>/', views.deleteBlog, name='delete-blog'),
+    # path('create-blog/', views.createBlog, name='create-blog'),
+    path('create-blog/', views.CreateBlog.as_view(), name='create-blog'),
+    # path('update-blog/<str:pk>/', views.updateBlog, name='update-blog'),
+    path('update-blog/<str:pk>/', views.UpdateBlog.as_view(), name='update-blog'),
+    # path('delete-blog/<str:pk>/', views.deleteBlog, name='delete-blog'),
+    path('delete-blog/<str:pk>/', views.DeleteBlog.as_view(), name='delete-blog'),
     
 ]
--- base/views.py
+++ base/views.py
@@ -3,57 +3,124 @@ from django.db.models import Q
 
 from .models import Blog, Comment
 from .forms import BlogForm
+from django.views.generic import View
 
 
-def home(request):
-    q = request.GET.get('q') if request.GET.get('q') != None else ''
-    blogs = Blog.objects.filter(
-        Q(topic__name__icontains=q) | 
-        Q(title__icontains=q) |
-        Q(body__icontains=q)
-        )
-    context = {
-        'blogs': blogs
-    }
-    return render(request, 'base/home.html', context)
+# def home(request):
+#     q = request.GET.get('q') if request.GET.get('q') != None else ''
+#     blogs = Blog.objects.filter(
+#         Q(topic__name__icontains=q) | 
+#         Q(title__icontains=q) |
+#         Q(body__icontains=q)
+#         )
 
-def blog(request, pk):
-    blog = Blog.objects.get(id=pk)
-    comments = Comment.objects.filter(blog=blog)  # Filter comments for this blog
-    context = {
-        'blog': blog,
-        'comments': comments,  # Add comments to the context
-    }
-    return render(request, 'base/blog.html', context)
+#     context = {
+#         'blogs': blogs,
+#     }
+#     return render(request, 'base/home.html', context)
 
-def createBlog(request):
-    form = BlogForm()
-    if request.method == 'POST':
+class Home(View):
+    def get(self, request):
+        q = request.GET.get('q') if request.GET.get('q') != None else ''
+        blogs = Blog.objects.filter(
+            Q(topic__name__icontains=q) | 
+            Q(title__icontains=q) |
+            Q(body__icontains=q)
+            )
+
+        context = {
+            'blogs': blogs,
+        }
+        return render(request, 'base/home.html', context)
+
+# def blog(request, pk):
+#     blog = Blog.objects.get(id=pk)
+#     comments = Comment.objects.filter(blog=blog)  # Filter comments for this blog
+#     context = {
+#         'blog': blog,
+#         'comments': comments,  # Add comments to the context
+#     }
+#     return render(request, 'base/blog.html', context)
+
+class BlogView(View):
+    def get(self, request, pk):
+        blog = Blog.objects.get(id=pk)
+        comments = Comment.objects.filter(blog=blog)  # Filter comments for this blog
+        context = {
+            'blog': blog,
+            'comments': comments,  # Add comments to the context
+        }
+        return render(request, 'base/blog.html', context)
+
+# def createBlog(request):
+#     form = BlogForm()
+#     if request.method == 'POST':
+#         form = BlogForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+
+#     context = {'form': form}
+#     return render(request, 'base/blog_form.html', context)
+
+class CreateBlog(View):
+    def get(self, request):
+        form = BlogForm()
+        context = {'form': form}
+        return render(request, 'base/blog_form.html', context)
+
+    def post(self, request):
         form = BlogForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
+        context = {'form': form}
+        return render(request, 'base/blog_form.html', context)
 
-    context = {'form': form}
-    return render(request, 'base/blog_form.html', context)
+# def updateBlog(request, pk):
+#     blog = Blog.objects.get(id=pk)
+#     form = BlogForm(instance=blog)
 
-def updateBlog(request, pk):
-    blog = Blog.objects.get(id=pk)
-    form = BlogForm(instance=blog)
+#     if request.method == 'POST':
+#         form = BlogForm(request.POST, instance=blog)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+        
+#     context = {'form': form}
+#     return render(request, 'base/blog_form.html', context)
 
-    if request.method == 'POST':
+class UpdateBlog(View):
+    def get(self, request, pk):
+        blog = Blog.objects.get(id=pk)
+        form = BlogForm(instance=blog)
+        context = {'form': form}
+        return render(request, 'base/blog_form.html', context)
+
+    def post(self, request, pk):
+        blog = Blog.objects.get(id=pk)
         form = BlogForm(request.POST, instance=blog)
         if form.is_valid():
             form.save()
             return redirect('home')
-        
-    context = {'form': form}
-    return render(request, 'base/blog_form.html', context)
+        context = {'form': form}
+        return render(request, 'base/blog_form.html', context)
 
-def deleteBlog(request, pk):
-    blog = Blog.objects.get(id=pk)
-    if request.method == 'POST':
+# def deleteBlog(request, pk):
+#     blog = Blog.objects.get(id=pk)
+#     if request.method == 'POST':
+#         blog.delete()
+#         return redirect('home')
+#     context = {'obj': blog}
+#     return render(request, 'base/delete.html', context)
+
+class DeleteBlog(View):
+    def get(self, request, pk):
+        blog = Blog.objects.get(id=pk)
+        context = {'obj': blog}
+        return render(request, 'base/delete.html', context)
+
+    def post(self, request, pk):
+        blog = Blog.objects.get(id=pk)
         blog.delete()
         return redirect('home')
-    context = {'obj': blog}
-    return render(request, 'base/delete.html', context)
