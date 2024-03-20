d212c4908cf09d186a4b6f93b9c127bd447dd54c
Jerom <jeromjomanthara@gmail.com>
2024-03-20 18:52:10 +0530

    perf(base): comment out class based views and use function based views

--- base/views.py
+++ base/views.py
@@ -8,77 +8,81 @@ from .forms import BlogForm
 ### function based views
 #############################################
 
-# def home(request):
-#     q = request.GET.get('q') if request.GET.get('q') != None else ''
-#     blogs = Blog.objects.filter(
-#         Q(topic__name__icontains=q) | 
-#         Q(title__icontains=q) |
-#         Q(body__icontains=q)
-#         )
+def home(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    blogs = Blog.objects.filter(
+        Q(topic__name__icontains=q) | 
+        Q(title__icontains=q) |
+        Q(body__icontains=q)
+        )
 
-#     context = {
-#         'blogs': blogs,
-#     }
-#     return render(request, 'base/home.html', context)
+    context = {
+            'blogs': blogs,
+        }
+    return render(request, 'base/home.html', context)
 
-# def blog(request, pk):
-#     blog = Blog.objects.get(id=pk)
-#     comments = Comment.objects.filter(blog=blog)  # Filter comments for this blog
-#     context = {
-#         'blog': blog,
-#         'comments': comments,  # Add comments to the context
-#     }
-#     return render(request, 'base/blog.html', context)
+def blog(request, pk):
+    blog = Blog.objects.get(id=pk)
+    comments = Comment.objects.filter(blog=blog)  # Filter comments for this blog
+    context = {
+        'blog': blog,
+        'comments': comments,  # Add comments to the context
+    }
+    return render(request, 'base/blog.html', context)
 
-# def createBlog(request):
-#     form = BlogForm()
-#     if request.method == 'POST':
-#         form = BlogForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
+def createBlog(request):
+    form = BlogForm()
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
 
-#     context = {'form': form}
-#     return render(request, 'base/blog_form.html', context)
+    context = {'form': form}
+    return render(request, 'base/blog_form.html', context)
 
-# def updateBlog(request, pk):
-#     blog = Blog.objects.get(id=pk)
-#     form = BlogForm(instance=blog)
+def updateBlog(request, pk):
+    blog = Blog.objects.get(id=pk)
+    form = BlogForm(instance=blog)
 
-#     if request.method == 'POST':
-#         form = BlogForm(request.POST, instance=blog)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
+    if request.method == 'POST':
+        form = BlogForm(request.POST, instance=blog)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
         
-#     context = {'form': form}
-#     return render(request, 'base/blog_form.html', context)
+    context = {'form': form}
+    return render(request, 'base/blog_form.html', context)
 
-# def deleteBlog(request, pk):
-#     blog = Blog.objects.get(id=pk)
-#     if request.method == 'POST':
-#         blog.delete()
-#         return redirect('home')
-#     context = {'obj': blog}
-#     return render(request, 'base/delete.html', context)
+def deleteBlog(request, pk):
+    blog = Blog.objects.get(id=pk)
+    if request.method == 'POST':
+        blog.delete()
+        return redirect('home')
+    context = {'obj': blog}
+    return render(request, 'base/delete.html', context)
 
 #############################################
 ### class based views
 #############################################
-from django.views.generic import View
-class Home(View):
-    def get(self, request):
-        q = request.GET.get('q') if request.GET.get('q') != None else ''
-        blogs = Blog.objects.filter(
-            Q(topic__name__icontains=q) | 
-            Q(title__icontains=q) |
-            Q(body__icontains=q)
-            )
+# from django.views.generic import View
+# class Home(View):
+#     def get(self, request):
+#         q = request.GET.get('q') if request.GET.get('q') != None else ''
+#         blogs = Blog.objects.filter(
+#             Q(topic__name__icontains=q) | 
+#             Q(title__icontains=q) |
+#             Q(body__icontains=q)
+#             )
+#         topics = Topic.objects.all()
+#         blog_count = blogs.count()
 
-        context = {
-            'blogs': blogs,
-        }
-        return render(request, 'base/home.html', context)
+#         context = {
+#             'blogs': blogs,
+#             'topics': topics,
+#             'blog_count': blog_count,
+#         }
+#         return render(request, 'base/home.html', context)
     
 # class BlogDetail(View):
 #     def get(self, request, pk):
@@ -135,32 +139,32 @@ class Home(View):
 ### class based views using specific views
 ##################################################
 
-from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+# from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
 
-class BlogDetail(DetailView):
-    model = Blog
-    template_name = 'base/blog.html'
+# class BlogDetail(DetailView):
+#     model = Blog
+#     template_name = 'base/blog.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        blog = self.get_object()
-        comments = Comment.objects.filter(blog=blog)  # Filter comments for this blog
-        context['comments'] = comments
-        return context
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         blog = self.get_object()
+#         comments = Comment.objects.filter(blog=blog)  # Filter comments for this blog
+#         context['comments'] = comments
+#         return context
 
-class CreateBlog(CreateView):
-    model = Blog
-    form_class = BlogForm
-    template_name = 'base/blog_form.html'
-    success_url = '/'  # Redirect to home page after successful creation
+# class CreateBlog(CreateView):
+#     model = Blog
+#     form_class = BlogForm
+#     template_name = 'base/blog_form.html'
+#     success_url = '/'  # Redirect to home page after successful creation
 
-class UpdateBlog(UpdateView):
-    model = Blog
-    form_class = BlogForm
-    template_name = 'base/blog_form.html'
-    success_url = '/'  # Redirect to home page after successful update
+# class UpdateBlog(UpdateView):
+#     model = Blog
+#     form_class = BlogForm
+#     template_name = 'base/blog_form.html'
+#     success_url = '/'  # Redirect to home page after successful update
 
-class DeleteBlog(DeleteView):
-    model = Blog
-    template_name = 'base/delete.html'
-    success_url = '/'  # Redirect to home page after successful deletion
+# class DeleteBlog(DeleteView):
+#     model = Blog
+#     template_name = 'base/delete.html'
+#     success_url = '/'  # Redirect to home page after successful deletion
