ccadf5ca7b106d02abc248c557b74670583a9804
Jerom <jeromjomanthara@gmail.com>
2024-03-28 09:07:56 +0530

    feat(base.admin): register custom user with admin

--- base/admin.py
+++ base/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Blog, Topic, Comment
+from .models import Blog, Topic, Comment, User
+
+admin.site.register(User)
 
 admin.site.register(Blog)
 admin.site.register(Topic)
